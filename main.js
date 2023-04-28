dda5cb8faa67cd9e261f6ca2e9c65a2f0e1b3429
肥田 幸久(hz190162wz) <hz190162wz@jindai.jp>
2023-04-28 12:56:10 +0900

    496

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 t = c.getContext("2d");
 L = [];
-b.onkeydown = e => L[e.keyCode] = 1;
-b.onkeyup = e => L[e.keyCode] = 0;
 for (i = 10; i--;) L[i] = Array(10).fill(5 - i);
 s = c => t.fillStyle = c;
 r = (x, y, w, h) => t.fillRect(x, y, w, h);
 px = 30;
+b.onkeydown = e => (k = e.keyCode - 38) ** 2 == 1 && -k < px && px < 60 - k && (px += k);
 bx = 50; by = 85;
 vx = vy = 1;
 setInterval(z => {
@@ -20,8 +19,6 @@ setInterval(z => {
         if (L[y = by / 10 | 0][x = bx / 10 | 0] > 0 || (px < bx && bx < px + 40 && 86 < by)) vy *= -1, L[y][x] = 0;
         r(bx, by, 4, 4);
 
-        if (L[37] && 0 < px) px--;
-        if (L[39] && px < 60) px++;
         r(px, 90, 40, 4)
     } else t.fillText("GAME OVER", 20, 55)
 }, 32)
