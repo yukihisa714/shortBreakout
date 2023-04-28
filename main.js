90a3109786cdb31de7a81519da7c3e6d8979bc0a
肥田 幸久(hz190162wz) <hz190162wz@jindai.jp>
2023-04-29 01:24:43 +0900

    472

--- main.js
+++ main.js
@@ -7,19 +7,19 @@ r = (x, y, w, h) => t.fillRect(x, y, w, h);
 a = n => n / o | 0;
 p = 30;
 b.onkeydown = e => (k = e.keyCode - 38) ** 2 == 1 && -k < p && p < 60 - k && (p += k);
-bx = 50; by = 85;
-vx = vy = 1;
+d = 50; e = 85;
+v = w = 1;
 setInterval(z => {
     s("#000");
     r(0, 0, 100, 100);
     s("#eee");
-    if (by < 94) {
+    if (e < 94) {
         for (i = 50; i--;)L[y = a(i)][x = i % o] && r(x * o, y * o, o, o);
 
-        bx += vx; by += vy;
-        if (bx < 0 || 96 < bx) vx *= - 1;
-        if (L[y = a(by)][x = a(bx)] > 0 || (p < bx && bx < p + 40 && 86 < by)) vy *= -1, L[y][x] = 0;
-        r(bx, by, 4, 4);
+        d += v; e += w;
+        if (d < 0 || 96 < d) v *= - 1;
+        if (L[y = a(e)][x = a(d)] > 0 || (p < d && d < p + 40 && 86 < e)) w *= -1, L[y][x] = 0;
+        r(d, e, 4, 4);
 
         r(p, 90, 40, 4)
     } else t.fillText("GAME OVER", 20, 55)
