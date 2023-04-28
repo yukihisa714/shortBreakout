e7bf1d3e1b11d6ab662d14d351ba8028465a5af9
肥田 幸久(hz190162wz) <hz190162wz@jindai.jp>
2023-04-29 04:24:19 +0900

    468

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 t = c.getContext("2d");
-o = 10;
 L = [];
-for (i = o; i--;) L[i] = Array(o).fill(5 - i);
+for (i = 9; i--;) L[i] = Array(9).fill(5 - i);
 s = c => t.fillStyle = c;
 r = (x, y, w, h) => t.fillRect(x, y, w, h);
-a = n => n / o | 0;
-p = 30;
-b.onkeydown = e => (k = e.keyCode - 38) ** 2 == 1 && -k < p && p < 60 - k && (p += k);
-d = 50; e = 85;
+a = n => n / 9 | 0;
+p = 25;
+b.onkeydown = e => (k = 38 - e.keyCode) ** 2 == 1 && k < p && p < 41 + k && (p -= k);
+d = 49; e = 60;
 v = w = 1;
 setInterval(z => {
     s("#000");
-    r(0, 0, 100, 100);
+    r(0, 0, 81, 81);
     s("#eee");
-    if (e < 94) {
-        for (i = 50; i--;)L[y = a(i)][x = i % o] && r(x * o, y * o, o, o);
+    if (e < 70) {
+        for (i = 45; i--;)L[y = a(i)][x = i % 9] && r(x * 9, y * 9, 9, 9);
 
         d += v; e += w;
-        if (d < 0 || 96 < d) v *= - 1;
-        if (L[y = a(e)][x = a(d)] > 0 || (p < d && d < p + 40 && 86 < e)) w *= -1, L[y][x] = 0;
+        if (d < 0 || 77 < d) v *= - 1;
+        if (!e || L[y = a(e)][x = a(d)] > 0 || (p < d && d < p + 40 && 66 < e)) w *= -1, L[y][x] = 0;
         r(d, e, 4, 4);
 
-        r(p, 90, 40, 4)
-    } else t.fillText("GAME OVER", 20, 55)
+        r(p, 70, 40, 4)
+    } else t.fillText("GAME OVER", 11, 45)
 }, 32)
