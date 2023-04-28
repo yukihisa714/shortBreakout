1ba3701c9c7f5b69186d1a849890c0c136d13d01
肥田 幸久(hz190162wz) <hz190162wz@jindai.jp>
2023-04-28 15:00:59 +0900

    492

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 t = c.getContext("2d");
+o = 10;
 L = [];
-for (i = 10; i--;) L[i] = Array(10).fill(5 - i);
+for (i = o; i--;) L[i] = Array(o).fill(5 - i);
 s = c => t.fillStyle = c;
 r = (x, y, w, h) => t.fillRect(x, y, w, h);
-px = 30;
-b.onkeydown = e => (k = e.keyCode - 38) ** 2 == 1 && -k < px && px < 60 - k && (px += k);
+a = n => n / o | 0;
+p = 30;
+b.onkeydown = e => (k = e.keyCode - 38) ** 2 == 1 && -k < p && p < 60 - k && (p += k);
 bx = 50; by = 85;
 vx = vy = 1;
 setInterval(z => {
@@ -12,13 +14,13 @@ setInterval(z => {
     r(0, 0, 100, 100);
     s("#eee");
     if (by < 94) {
-        for (i = 50; i--;)L[y = i / 10 | 0][x = i % 10] && r(x * 10, y * 10, 10, 10);
+        for (i = 50; i--;)L[y = a(i)][x = i % o] && r(x * o, y * o, o, o);
 
         bx += vx; by += vy;
         if (bx < 0 || 96 < bx) vx *= - 1;
-        if (L[y = by / 10 | 0][x = bx / 10 | 0] > 0 || (px < bx && bx < px + 40 && 86 < by)) vy *= -1, L[y][x] = 0;
+        if (L[y = a(by)][x = a(bx)] > 0 || (p < bx && bx < p + 40 && 86 < by)) vy *= -1, L[y][x] = 0;
         r(bx, by, 4, 4);
 
-        r(px, 90, 40, 4)
+        r(p, 90, 40, 4)
     } else t.fillText("GAME OVER", 20, 55)
 }, 32)
